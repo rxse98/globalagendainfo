656c8d51d1cb2c591cb8737c9c4e970172fb0674
Stanislav <98thrxse@gmail.com>
2022-02-20 21:02:45 +0200

    checking serverWS ip

--- index.js
+++ index.js
@@ -47,7 +47,14 @@ function clientTCP() {
 
 function serverWS() {
   const WebSocket = require('ws');
-  const wsServer = new WebSocket.Server({port: 9000});
+  const wsServer = new WebSocket.Server(
+    {
+      port: 9000,
+      verifyClient(info) {
+        console.log(info.req.connection.remoteAddress);
+        return true;
+      },
+  });
   
   wsServer.on('connection', onConnect);
 }
