ee86b4452a99eabe5825fe637d4e2229bae66702
Stanislav <98thrxse@gmail.com>
2022-02-21 00:47:59 +0200

    edited info text

--- js/dialog.js
+++ js/dialog.js
@@ -1,11 +1,15 @@
 function clickInfo() {
     let text = document.getElementById("text")
-    text.innerText = "\"More info: "
+    text.innerText = "\"Want to know more info? Join us: "
     
-    let link = document.createElement('a')
-    link.innerText = "*LINK* \""
+    let link = document.createElement("a")
+    link.innerText = "*LINK*"
     link.href = "https://discord.me/GA1"
     text.appendChild(link)
+
+    let text2 = document.createElement("p")
+    text2.innerText = " , Agent.\""
+    text.appendChild(text2)
 }
 
 function clickRefresh() {
