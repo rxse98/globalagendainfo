a753d37ca8e7e1be9dd164b8474a8c70b0327b66
Stanislav <98thrxse@gmail.com>
2022-02-23 21:44:33 +0200

    added comments to src

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// include libs
 const express = require('express')
 const WebSocket = require('ws');
 const net = require('net');
 
+// creates server to host site
 function expressServer() {
 
+  // express server port
   const PORT = process.env.PORT || 3000;
-  const INDEX = '/index.html';
   
+  // index.html was sent, all files in directory root should be loaded, start express server
   const server = express()
-    .use(express.static(__dirname), (req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .use(express.static(__dirname), (req, res) => res.sendFile('/index.html', { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
   
   console.log('Express server started.');
@@ -18,18 +21,25 @@ function expressServer() {
 
 expressServer()
 
+// ws server sends and receives data from tcp client, then returns data back to ws client
 function serverWS(server) {
+
+  // create ws server, all opts are the same as express server
   const wsServer = new WebSocket.Server({ server });
 
-  console.log('Websocket server started.');
-
+  // ws server reacts on ws client
   wsServer.on('connection', function(wsClient) {
-    console.log('Websocket sending temporary response.');
-    wsClient.send("\"Refreshing server status, Agent..\"");
 
+    // ws server receives request from ws client
     wsClient.on('message', function(request) {
       console.log('Websocket server received request.');
+
+      // ws server receives request from ws client to refresh GA server status
       if (request == "official") {
+        // ws server sends temporary data to ws client
+        console.log('Websocket sending temporary response.');
+        wsClient.send("\"Refreshing server status, Agent..\"");
+        // ws server creates tcp client and waits for response, then ws server sends GA server status to ws client
         clientOfficialTCP().then(online => {
           wsClient.send(online)
         }).catch(offline => {
@@ -40,18 +50,26 @@ function serverWS(server) {
   });
 }
 
+// tcp client sends data to GA server and promises that response will return to ws server as tcp client receives it
 function clientOfficialTCP() {
   return new Promise((resolve, reject) => {
+
+    // create tcp client
     const client = new net.Socket();
 
+    // tcp client request to check GA server status
     const request = Buffer.from([0x0a, 0x00, 0x3b, 0x00, 0x01, 0x00, 0x36, 0x05, 0x01, 0x00, 0x05, 0x01])
+
+    // GA server is online if tcp client receives required response
     const required = Buffer.from([0x1c, 0x00, 0x3b, 0x00, 0x02, 0x00, 0x36, 0x05, 0x01, 0x00, 0x05, 0x01, 0x73, 0x04])
 
+    // tcp client sends request to GA server
     client.connect(9000, '107.150.130.77', function() {
-      console.log('TCP client connected, sending request.');
+      console.log('TCP client sending request.');
       client.write(request)
     });
 
+    // tcp client receives response from GA server and sends it to ws server
     client.on('data', function(response) {
       console.log('TCP client received response.');
 
@@ -62,13 +80,16 @@ function clientOfficialTCP() {
       }
     });
 
+    // tcp client connection timeout
     client.setTimeout(5000);
 
+    // tcp client sends response to ws server after timeout, GA server is offline or user's internet connection is low
     client.on('timeout', () => {
       console.log('TCP client timeout.');
       reject("\"Refreshed, the official server is offline, Agent.\"")
     });
 
+    // tcp client connection error, can't reach GA server
     client.on('error', function() {
       console.log('TCP client error.');
       reject("\"Refreshed, the official server is offline, Agent.\"")
--- js/non-obf/dialog-src.js
+++ js/non-obf/dialog-src.js
@@ -1,35 +1,47 @@
+// onClick: Info
 function clickInfo() {
+    // change dialog text
     let text = document.getElementById("text")
     text.innerText = "\"Want to know more info? Join us: "
     
+    // add discord link to text
     let link = document.createElement("a")
     link.classList.add("link")
     link.innerText = "*LINK*"
     link.href = "https://discord.me/GA1"
     text.appendChild(link)
 
+    // add text after discord link
     let text2 = document.createElement("label")
     text2.innerText = " , Agent.\""
     text.appendChild(text2)
 }
 
+// onClick: Refresh server status
 function clickOfficialRefresh() {
+    // ws client route to connect, ws server host = express server host, difference is that we should replace http on ws
     var HOST = location.origin.replace(/^http/, 'ws')
     var ws = new WebSocket(HOST);
 
+    // ws client sending request to ws server on connect
     ws.onopen = () => {
-        console.log("Websocket client connected, sending request.")
+        console.log("Websocket sending request.")
+        // ws client sends request to ws server about refreshing GA server status
         ws.send("official")
     }
 
+    // ws client receives response from ws server about GA server status
     ws.onmessage = (event) => {
         console.log("Websocket client received data.")
+        // change dialog text
         let text = document.getElementById("text")
         text.innerText = event.data
     }
 }
 
+// onClick: Private Servers
 function clickPrivate() {
+    // change dialog text
     let text = document.getElementById("text")
     text.innerText = "\"Now, we can't make a private server, no one has the server-side files, but you can help us to recreate them, Agent.\""
 }
